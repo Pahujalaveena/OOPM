5d0f5635a991d1f897d44c316ac25007cab920ec
Pahujalaveena <113932098+Pahujalaveena@users.noreply.github.com>
2022-12-14 17:55:39 +0000

    Add files via upload

---
+++ ++operator.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class person
+{
+	private:
+		int weight;
+	public:
+		person(int w=0)
+		{
+			weight=w;
+		}
+	void printweight()
+	{
+		cout<<"Weight is "<<weight<<endl;
+	}
+	void operator++()
+	{
+		++weight;
+	}
+};
+int main()
+{
+	person Jon(78);
+	++Jon;
+	Jon.printweight();
+}
